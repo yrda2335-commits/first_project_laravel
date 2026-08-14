200176a5d3fe0516ab3040b3af1d7a1209970d65
yrda2335-commits <yrda2335@gmail.com>
2026-08-14 21:35:13 +0300

    feat: add custom 404 error page for workshops

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,13 @@
+<x-layout>
+    <x-card>
+        <h1>Workshop Not Found</h1>
+
+        <p>
+            The workshop you are looking for does not exist.
+        </p>
+
+        <x-button href="/workshops">
+            Back to Workshops
+        </x-button>
+    </x-card>
+</x-layout>
