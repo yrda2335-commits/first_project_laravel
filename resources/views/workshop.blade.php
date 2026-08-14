a2822542993a387801d56abd09d18d159921bcbd
yrda2335-commits <yrda2335@gmail.com>
2026-08-14 19:33:33 +0300

    feat: add instructor profile section to workshop view

--- resources/views/workshop.blade.php
+++ resources/views/workshop.blade.php
@@ -5,4 +5,7 @@
     <p>Date: {{ $workshop['date'] }}</p>
     <p>Level: {{ $workshop['level'] }}</p>
     <p>{{ $workshop['description'] }}</p>
+    <h2>Instructor Profile</h2>
+
+<p>{{ $workshop['instructor'] }}</p>
 </x-layout>
