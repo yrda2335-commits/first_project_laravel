21e9993549e1957e9f613176c2ff9b5312060778
yrda2335-commits <yrda2335@gmail.com>
2026-08-14 20:55:55 +0300

    feat: add statistics display to about page

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,3 +1,14 @@
 <x-layout>
     <h1>About DevPulse</h1>
+     @php
+        $stats = [
+            'Total Students' => 1200,
+            'Workshops Held' => 48,
+            'Expert Instructors' => 15,
+        ];
+    @endphp
+
+    @foreach ($stats as $label => $value)
+        <p>{{ $label }}: {{ $value }}</p>
+    @endforeach
 </x-layout>
