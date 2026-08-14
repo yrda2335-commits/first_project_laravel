69e75bf82b1b7b541eb89abcde8205707593aeb3
yrda2335-commits <yrda2335@gmail.com>
2026-08-14 18:51:53 +0300

    update nav-link component to conditionally apply active class based on route

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -1,3 +1,8 @@
-<a {{ $attributes->merge(['class' => 'px-3 py-2 rounded-lg hover:bg-slate-800 transition']) }}>
+@props(['active' => ''])
+<a  {{ $attributes->merge([
+    'class' => request()->routeIs($active) 
+        ? 'px-3 py-2 rounded-lg bg-slate-700 transition'
+        : 'px-3 py-2 rounded-lg hover:bg-slate-800 transition'
+]) }} >
     {{ $slot }}
     </a>
